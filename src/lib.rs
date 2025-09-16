0cc50215275676435921e9033f3235756f235a49
Matheus Alves <mathalves@proton.me>
2025-09-15 21:41:12 -0300

    handle booleans and null

--- src/lib.rs
+++ src/lib.rs
@@ -5,18 +5,20 @@ pub fn fix(input: impl Into<String>) -> String {
 struct Parser {
     input: String,
     result: String,
+    current: char,
     state: ParserState,
     need_close: Vec<char>,
 }
 
 enum ParserState {
     ValueStart,
-    Object,
     True,
     False,
     Null,
+    Object,
     Number,
     String,
+    ValueEnd,
 }
 
 impl Parser {
@@ -24,6 +26,7 @@ impl Parser {
         Self {
             input: input.into(),
             result: String::new(),
+            current: '\0',
             state: ParserState::ValueStart,
             need_close: Vec::new(),
         }
@@ -33,8 +36,13 @@ impl Parser {
         self.input = self.input.trim().to_string();
 
         for ch in self.input.chars().collect::<Vec<char>>() {
+            self.current = ch;
+
             match self.state {
-                ParserState::ValueStart => self.value_start(ch),
+                ParserState::ValueStart => self.value_start(),
+                ParserState::True => self.handle_true(),
+                ParserState::False => self.handle_false(),
+                ParserState::Null => self.handle_null(),
 
                 _ => todo!(),
             }
@@ -43,12 +51,8 @@ impl Parser {
         self.result.to_owned()
     }
 
-    fn value_start(&mut self, ch: char) {
-        match ch {
-            '{' => {
-                self.state = ParserState::Object;
-            }
-
+    fn value_start(&mut self) {
+        match self.current {
             't' => {
                 self.state = ParserState::True;
             }
@@ -59,6 +63,10 @@ impl Parser {
 
             'n' => {
                 self.state = ParserState::Null;
+            }
+
+            '{' => {
+                self.state = ParserState::Object;
             }
 
             '-' | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
@@ -76,5 +84,20 @@ impl Parser {
 
             _ => {}
         }
+    }
+
+    fn handle_true(&mut self) {
+        self.result.push_str("true");
+        self.state = ParserState::ValueEnd;
+    }
+
+    fn handle_false(&mut self) {
+        self.result.push_str("false");
+        self.state = ParserState::ValueEnd;
+    }
+
+    fn handle_null(&mut self) {
+        self.result.push_str("null");
+        self.state = ParserState::ValueEnd;
     }
 }
