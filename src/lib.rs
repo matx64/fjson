aa9172d53273e1e0da81f795d5b594008634090c
Matheus Alves <mathalves@proton.me>
2025-09-22 15:37:46 -0300

    switch to new parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,212 +1,165 @@
-mod lib2;
+use std::collections::HashMap;
 
 pub fn fix(input: impl Into<String>) -> String {
-    Parser::new(input).parse_and_fix()
+    let json = Parser::new(input).parse_and_fix();
+    json.stringify()
+}
+
+enum Json {
+    Null,
+    True,
+    False,
+    Number(String),
+    String(String),
+    Array(Vec<Json>),
+    Object(HashMap<String, Json>),
 }
 
 struct Parser {
-    input: String,
-    result: String,
-    state: ParserState,
-    current: char,
-    lexeme: String,
-    need_close: Vec<char>,
-}
-
-enum ParserState {
-    ValueStart,
-    Integer,
-    Float,
-    String,
-    Object,
-    ObjectKey,
-    ValueEnd,
+    chars: Vec<char>,
+    i: usize,
 }
 
 impl Parser {
     pub fn new(input: impl Into<String>) -> Self {
         Self {
-            input: input.into(),
-            result: String::new(),
-            state: ParserState::ValueStart,
-            current: '\0',
-            lexeme: String::new(),
-            need_close: Vec::new(),
+            chars: input.into().trim().chars().collect(),
+            i: 0,
         }
     }
 
-    pub fn parse_and_fix(&mut self) -> String {
-        self.input = self.input.trim().to_string();
-
-        for ch in self.input.chars().collect::<Vec<char>>() {
-            self.current = ch;
-
-            match self.state {
-                ParserState::ValueStart => self.handle_value_start(),
-
-                ParserState::Integer => self.handle_integer(),
-                ParserState::Float => self.handle_float(),
-
-                ParserState::String => self.handle_string(),
-
-                ParserState::Object => self.handle_object(),
-                ParserState::ObjectKey => self.handle_object_key(),
-
-                ParserState::ValueEnd => self.handle_value_end(),
-            }
-        }
-
-        self.handle_end();
-
-        if self.result.is_empty() {
-            self.result = Self::new(format!("\"{}\"", self.input)).parse_and_fix();
-        }
-        self.result.to_owned()
+    fn peek(&mut self) -> Option<char> {
+        self.chars.get(self.i).copied()
     }
 
-    fn handle_value_start(&mut self) {
-        match self.current {
-            't' | 'T' => {
-                self.handle_true();
-            }
+    fn next(&mut self) -> Option<char> {
+        let c = self.peek();
+        if c.is_some() {
+            self.i += 1;
+        }
+        c
+    }
 
-            'f' | 'F' => {
-                self.handle_false();
-            }
-
-            'n' | 'N' => {
-                self.handle_null();
-            }
-
-            '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '-' => {
-                self.state = ParserState::Integer;
-                self.lexeme = String::from(self.current);
-            }
-
-            '.' => {
-                self.state = ParserState::Float;
-                self.lexeme = String::from("0.");
-            }
-
-            '"' => {
-                self.state = ParserState::String;
-                self.lexeme = String::from(self.current);
-                self.need_close.push('"');
-            }
-
-            '{' => {
-                self.state = ParserState::Object;
-                self.result.push(self.current);
-                self.need_close.push('}');
-            }
-
-            '[' => {
-                self.result.push(self.current);
-                self.need_close.push(']');
-            }
-
-            ']' => {
-                if let Some(last) = self.need_close.last()
-                    && *last == ']'
-                {
-                    self.result.push(self.current);
-                    self.need_close.pop();
-                }
-            }
-
-            _ => {}
+    fn skip_whitespace(&mut self) {
+        while let Some(c) = self.peek()
+            && c.is_whitespace()
+        {
+            self.next();
         }
     }
 
-    fn handle_true(&mut self) {
-        self.result.push_str("true");
-        self.state = ParserState::ValueEnd;
+    pub fn parse_and_fix(&mut self) -> Json {
+        self.parse_value()
     }
 
-    fn handle_false(&mut self) {
-        self.result.push_str("false");
-        self.state = ParserState::ValueEnd;
-    }
+    fn parse_value(&mut self) -> Json {
+        self.skip_whitespace();
 
-    fn handle_null(&mut self) {
-        self.result.push_str("null");
-        self.state = ParserState::ValueEnd;
-    }
+        if let Some(c) = self.peek() {
+            match c {
+                'n' | 'N' | 't' | 'T' | 'f' | 'F' => self.parse_static(),
 
-    fn handle_integer(&mut self) {
-        match self.current {
-            '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
-                self.lexeme.push(self.current);
+                val if val.is_ascii_digit() || val == '-' => self.parse_number(),
+                '.' => self.parse_float("0"),
+
+                '"' => self.parse_string(),
+
+                '[' => self.parse_array(),
+
+                '{' => self.parse_object(),
+
+                _ => Json::Null,
             }
-
-            'e' | 'E' => {
-                if !self.lexeme.contains('e') {
-                    self.lexeme.push('e');
-                }
-            }
-
-            '+' | '-' => {
-                if let Some(last) = self.lexeme.chars().last()
-                    && last == 'e'
-                {
-                    self.lexeme.push(self.current);
-                }
-            }
-
-            '.' => {
-                if let Some(last) = self.lexeme.chars().last()
-                    && last == '-'
-                {
-                    self.lexeme.push('0');
-                }
-                self.lexeme.push(self.current);
-                self.state = ParserState::Float;
-            }
-
-            _ => {
-                self.normalize_number();
-                self.result.push_str(&self.lexeme);
-                self.handle_value_end();
-            }
+        } else {
+            Json::Null
         }
     }
 
-    fn handle_float(&mut self) {
-        match self.current {
-            '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
-                self.lexeme.push(self.current);
-            }
-
-            'e' | 'E' => {
-                if !self.lexeme.contains('e') {
-                    self.lexeme.push('e');
-                }
-            }
-
-            '+' | '-' => {
-                if let Some(last) = self.lexeme.chars().last()
-                    && last == 'e'
-                {
-                    self.lexeme.push(self.current);
-                }
-            }
-
-            '.' => {}
-
-            _ => {
-                self.normalize_number();
-                self.result.push_str(&self.lexeme);
-                self.handle_value_end();
-            }
+    fn parse_static(&mut self) -> Json {
+        match self.next().unwrap().to_ascii_lowercase() {
+            'n' => Json::Null,
+            't' => Json::True,
+            _ => Json::False,
         }
     }
 
-    fn normalize_number(&mut self) {
+    fn parse_number(&mut self) -> Json {
+        let mut lex = String::from(self.next().unwrap());
+
+        while let Some(c) = self.next() {
+            match c {
+                val if val.is_ascii_digit() => {
+                    lex.push(c);
+                }
+
+                'e' | 'E' => {
+                    if !lex.contains('e') {
+                        lex.push('e');
+                    }
+                }
+
+                '+' | '-' => {
+                    if let Some(last) = lex.chars().last()
+                        && last == 'e'
+                    {
+                        lex.push(c);
+                    }
+                }
+
+                '.' => {
+                    if let Some(last) = lex.chars().last()
+                        && last == '-'
+                    {
+                        lex.push('0');
+                    }
+                    return self.parse_float(&lex);
+                }
+
+                _ => {}
+            }
+        }
+
+        Json::Number(self.normalize_number(lex))
+    }
+
+    fn parse_float(&mut self, int_part: &str) -> Json {
+        let mut lex = format!("{}{}", int_part, self.next().unwrap());
+
+        while let Some(c) = self.next() {
+            match c {
+                val if val.is_ascii_digit() => {
+                    lex.push(c);
+                }
+
+                'e' | 'E' => {
+                    if !lex.contains('e') {
+                        lex.push('e');
+                    }
+                }
+
+                '+' | '-' => {
+                    if let Some(last) = lex.chars().last()
+                        && last == 'e'
+                    {
+                        lex.push(c);
+                    }
+                }
+
+                '.' => {}
+
+                _ => {}
+            }
+        }
+
+        Json::Number(self.normalize_number(lex))
+    }
+
+    fn normalize_number(&mut self, lex: String) -> String {
         let mut result = {
-            if self.lexeme.starts_with('-') {
-                self.lexeme[1..].to_string()
+            if lex.starts_with('-') {
+                lex.strip_prefix('-').unwrap().to_string()
             } else {
-                self.lexeme.clone()
+                lex.clone()
             }
         };
 
@@ -229,7 +182,7 @@ impl Parser {
             }
         }
 
-        if self.lexeme.starts_with('-') {
+        if lex.starts_with('-') {
             result.insert(0, '-');
         }
 
@@ -237,150 +190,137 @@ impl Parser {
             result = String::from('0');
         }
 
-        self.lexeme = result;
+        result
     }
 
-    fn handle_string(&mut self) {
-        match self.current {
-            '"' => {
-                if let Some(last) = self.lexeme.chars().last()
-                    && last == '\\'
-                {
-                    self.lexeme.push(self.current);
-                } else {
-                    self.lexeme.push(self.current);
-                    self.result.push_str(&self.lexeme);
-                    self.need_close.pop();
-                    self.state = ParserState::ValueEnd;
-                }
-            }
+    fn parse_string(&mut self) -> Json {
+        let mut lex = String::new();
 
-            _ => {
-                self.lexeme.push(self.current);
-            }
-        }
-    }
-
-    fn handle_object(&mut self) {
-        match self.current {
-            '"' => {
-                self.lexeme = String::from(self.current);
-                self.need_close.push('"');
-                self.state = ParserState::ObjectKey;
-            }
-
-            '}' => {
-                if let Some(last) = self.result.chars().last()
-                    && last == ','
-                {
-                    self.result.pop();
-                }
-                self.result.push('}');
-                self.need_close.pop();
-                self.state = ParserState::ValueEnd;
-            }
-
-            val => {
-                if !val.is_whitespace() {
-                    self.handle_value_end();
-                }
-            }
-        }
-    }
-
-    fn handle_object_key(&mut self) {
-        match self.current {
-            '"' => {
-                self.lexeme.push_str("\":");
-                self.result.push_str(&self.lexeme);
-                self.need_close.pop();
-                self.state = ParserState::ValueStart;
-            }
-
-            val => {
-                if !val.is_whitespace() {
-                    self.lexeme.push(self.current);
-                }
-            }
-        }
-    }
-
-    fn handle_value_end(&mut self) {
-        self.state = ParserState::ValueEnd;
-
-        if let Some(next_to_close) = self.need_close.last() {
-            if self.current == *next_to_close {
-                self.result.push(self.current);
-                self.need_close.pop();
-            } else if self.current == ',' {
-                if *next_to_close == '}' {
-                    self.result.push(self.current);
-                    self.state = ParserState::Object;
-                } else if *next_to_close == ']' {
-                    self.result.push(self.current);
-                    self.state = ParserState::ValueStart;
-                }
-            }
-        }
-    }
-
-    fn handle_end(&mut self) {
-        match self.state {
-            ParserState::ValueStart => {
-                if let Some(next_to_close) = self.need_close.last()
-                    && let Some(last) = self.result.chars().last()
-                {
-                    if last == ',' {
-                        self.result.pop();
-                    } else if *next_to_close == '}' {
-                        self.result.push_str("null");
+        self.next();
+        while let Some(c) = self.next() {
+            match c {
+                '"' => {
+                    if let Some(last) = lex.chars().last()
+                        && last == '\\'
+                    {
+                        lex.push(c);
+                    } else {
+                        break;
                     }
                 }
-            }
 
-            ParserState::Integer | ParserState::Float => {
-                self.normalize_number();
-                self.result.push_str(&self.lexeme);
-            }
-
-            ParserState::String => {
-                if self.lexeme.ends_with('\\') {
-                    self.lexeme.pop();
+                _ => {
+                    lex.push(c);
                 }
-
-                if self.lexeme.len() == 1 {
-                    self.lexeme.push('"');
-                } else {
-                    self.lexeme.push_str(" (TRUNCATED)\"");
-                }
-
-                self.result.push_str(&self.lexeme);
-                self.need_close.pop();
             }
-
-            ParserState::Object => {
-                if let Some(last) = self.result.chars().last()
-                    && last == ','
-                {
-                    self.result.pop();
-                }
-                self.result.push('}');
-                self.need_close.pop();
-            }
-
-            ParserState::ObjectKey => {
-                self.lexeme.push('"');
-                self.result.push_str(&self.lexeme);
-                self.need_close.pop();
-
-                self.result.push_str(": null}");
-            }
-
-            _ => {}
         }
 
-        while let Some(ch) = self.need_close.pop() {
-            self.result.push(ch);
+        Json::String(lex)
+    }
+
+    fn parse_array(&mut self) -> Json {
+        let mut arr = Vec::new();
+
+        self.next();
+        self.skip_whitespace();
+
+        if self.peek() == Some(']') {
+            return Json::Array(arr);
+        }
+
+        loop {
+            arr.push(self.parse_value());
+
+            self.skip_whitespace();
+            match self.peek() {
+                Some(',') => {
+                    self.next();
+                }
+
+                Some(']') | None => {
+                    self.next();
+                    break;
+                }
+
+                _ => {}
+            }
+        }
+
+        Json::Array(arr)
+    }
+
+    fn parse_object(&mut self) -> Json {
+        let mut obj = HashMap::new();
+
+        self.next();
+
+        loop {
+            self.skip_whitespace();
+
+            if let Some(c) = self.peek() {
+                match c {
+                    '}' => {
+                        self.next();
+                        break;
+                    }
+
+                    '"' => {
+                        let key = match self.parse_string() {
+                            Json::String(s) => s,
+                            _ => unreachable!(),
+                        };
+
+                        obj.insert(key, self.parse_value());
+                    }
+
+                    _ => {}
+                }
+            }
+        }
+
+        Json::Object(obj)
+    }
+}
+
+impl Json {
+    pub fn stringify(self) -> String {
+        match self {
+            Self::Null => "null".to_string(),
+            Self::True => "true".to_string(),
+            Self::False => "false".to_string(),
+
+            Self::Number(val) => val,
+            Self::String(val) => val,
+
+            Self::Array(arr) => {
+                let mut result = String::from('[');
+
+                for val in arr {
+                    result.push_str(&format!("{},", val.stringify()));
+                }
+
+                if result.ends_with(',') {
+                    result.pop();
+                }
+
+                result.push(']');
+                result
+            }
+
+            Self::Object(obj) => {
+                let mut result = String::from('{');
+
+                for (key, val) in obj {
+                    result.push_str(&format!("\"{}\":{},", key, val.stringify()));
+                }
+
+                if result.ends_with(',') {
+                    result.pop();
+                }
+
+                result.push('}');
+                result
+            }
         }
     }
 }
